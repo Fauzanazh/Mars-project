f821cc6902a35346e19301c359b1d500d64a3f7d
Fauzanazh <131636913+Fauzanazh@users.noreply.github.com>
2023-06-08 12:51:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,29 +14,27 @@ client = MongoClient (MONGODB_URI)
 db = client ["dbsparta"]
 app = Flask (__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    # sample_receive = request.form['sample_give']
-    name_receive = request.form["name_give"]
-    comment_receive = request.form["comment_give"]
+@app.route("/mars", methods=["POST"])
+def web_mars_post():
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
     doc = {
-        "name": name_receive,
-        "comment": comment_receive,
+        'name': name_receive,
+        'address': address_receive,
+        'size': size_receive
     }
-    db.fanmessages.insert_one(doc)
-    return jsonify({"msg": "POST request!"})
+    db.orders.insert_one(doc)
+    return jsonify({'msg': 'complete!'})
 
+@app.route("/mars", methods=["GET"])
+def web_mars_get():
+    orders_list = list(db.orders.find({}, {'_id': False}))
+    return jsonify({'orders': orders_list})
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    message_list = list(db.fanmessages.find({}, {"_id": False}))
-    return jsonify({"messages": message_list})
-
-
-if __name__ == "__main__":
-    app.run("0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
